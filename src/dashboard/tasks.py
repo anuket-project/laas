03097ee368e48395c47a33742fa72f5b920d3e65
Trevor Bramwell <tbramwell@linuxfoundation.org>
2018-02-02 13:37:23 -0800

    Remove the Booking Notification Cleanup Task
    These are created by the notification django-app, but that app is no
    longer used or installed.
    
    This change should also keep the worker container from continuously
    restarting.
    
    Change-Id: Ifbdf15e7011375f3818179048ec6be4a83a53917
    Signed-off-by: Trevor Bramwell <tbramwell@linuxfoundation.org>

--- src/dashboard/tasks.py
+++ src/dashboard/tasks.py
@@ -14,11 +14,9 @@ from celery import shared_task
 from django.utils import timezone
 
 from jenkins.models import JenkinsStatistic
-from notification.models import BookingNotification
 
 
 @shared_task
 def database_cleanup():
     now = timezone.now()
     JenkinsStatistic.objects.filter(timestamp__lt=now - timedelta(weeks=4)).delete()
-    BookingNotification.objects.filter(submit_time__lt=now - timedelta(weeks=4)).delete()
