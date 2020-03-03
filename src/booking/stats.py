aaff0c1a5abc850db49dc72a79abb581be8cfcfc
Sean <ssmith@iol.unh.edu>
2020-03-03 10:54:20 -0500

    Fixed a few bugs for the stats functions and created a few tests.
    Signed-off-by: Sean <ssmith@iol.unh.edu>
    Change-Id: I2e4598811bddabe5b7447c3a92d39d16acb77a03
    Signed-off-by: Sean <ssmith@iol.unh.edu>

--- src/booking/stats.py
+++ src/booking/stats.py
@@ -1,5 +1,5 @@
 ##############################################################################
-# Copyright (c) 2018 Parker Berberian, Sawyer Bergeron, and others.
+# Copyright (c) 2020 Parker Berberian, Sawyer Bergeron, Sean Smith and others.
 #
 # All rights reserved. This program and the accompanying materials
 # are made available under the terms of the Apache License, Version 2.0
@@ -7,7 +7,7 @@
 # http://www.apache.org/licenses/LICENSE-2.0
 ##############################################################################
 from booking.models import Booking
-import datetime
+from datetime import datetime, timedelta
 import pytz
 
 
@@ -18,43 +18,41 @@ class StatisticsManager(object):
         """
         Calculate Booking usage data points.
 
-        Will return a dictionary of names and 2-D array of x and y data points.
-        e.g. {"plot1": [["x1", "x2", "x3"],["y1", "y2", "y3]]}
-        x values will be dates in string
-        every change (booking start / end) will be reflected,
-        instead of one data point per day
-        y values are the integer number of bookings/users active at
-        some point in the given date span is the number of days to plot.
-        The last x value will always be the current time
+        Gathers all active bookings that fall in interval [(now - span), (now + 1 week)].
+        x data points are every 12 hours
+        y values are the integer number of bookings/users active at time
         """
-        data = []
+
         x = []
         y = []
         users = []
-        now = datetime.datetime.now(pytz.utc)
-        delta = datetime.timedelta(days=span)
-        end = now - delta
-        bookings = Booking.objects.filter(start__lte=now, end__gte=end).prefetch_related("collaborators")
-        for booking in bookings:  # collect data from each booking
-            user_list = [u.pk for u in booking.collaborators.all()]
-            user_list.append(booking.owner.pk)
-            data.append((booking.start, 1, user_list))
-            data.append((booking.end, -1, user_list))
 
-        # sort based on time
-        data.sort(key=lambda i: i[0])
+        now = datetime.now(pytz.utc)
+        delta = timedelta(days=span)
+        start = now - delta
+        end = now + timedelta(weeks=1)
 
-        # collect data
-        count = 0
-        active_users = {}
-        for datum in data:
-            x.append(str(datum[0]))  # time
-            count += datum[1]  # booking count
-            y.append(count)
-            for pk in datum[2]:  # maintain count of each user's active bookings
-                active_users[pk] = active_users.setdefault(pk, 0) + datum[1]
-                if active_users[pk] == 0:
-                    del active_users[pk]
-            users.append(len([x for x in active_users.values() if x > 0]))
+        bookings = Booking.objects.filter(
+            start__lte=end,
+            end__gte=start
+        ).prefetch_related("collaborators")
+
+        # get data
+        while start <= end:
+            active_users = 0
+
+            books = bookings.filter(
+                start__lte=start,
+                end__gte=start
+            ).prefetch_related("collaborators")
+
+            for booking in books:
+                active_users += booking.collaborators.all().count() + 1
+
+            x.append(str(start))
+            y.append(books.count())
+            users.append(active_users)
+
+            start += timedelta(hours=12)
 
         return {"booking": [x, y], "user": [x, users]}
