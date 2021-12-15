4d0cbdfaca47e0ed1fb799cf5ae754c7f167a874
Sawyer Bergeron <sbergeron@iol.unh.edu>
2021-12-15 14:34:29 -0500

    Add migration to mark cifiles as generated vs user-provided
    Signed-off-by: Sawyer Bergeron <sbergeron@iol.unh.edu>
    Change-Id: I2a7011f26a53dd1e56ed8c9853fb4a4b31cc863b

---
+++ src/api/migrations/0022_add_cifile_generated_field.py
@@ -0,0 +1,14 @@
+from django.db import migrations, models
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ('api', '0018_cloudinitfile'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name="CloudInitFile",
+            name="generated",
+            field=models.BooleanField(default=False)
+        ),
+    ]
