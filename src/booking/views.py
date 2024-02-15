a974a865d8e187acf9e650d06982635ea4b07580
Justin Choquette <jchoquette@iol.unh.edu>
2024-02-15 14:59:50 -0500

    Add mailto link back

--- src/booking/views.py
+++ src/booking/views.py
@@ -104,6 +104,7 @@ def booking_detail_view(request, booking_id):
             "status": statuses,
             "collab_string": ", ".join(map(str, booking.collaborators.all())),
             "ipa_username": profile.ipa_username,
+            "contact_email": Lab.objects.filter(name="UNH_IOL").first().contact_email
         }
 
         return render(request, "booking/booking_detail.html", context)
